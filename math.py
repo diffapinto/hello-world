debb925d042718470a2c6d56926d04f9d2fe0faa
diffapinto <diffa_pinto@persistent.com>
2021-08-29 09:55:31 +0530

    Added func header

--- math.py
+++ math.py
@@ -1,12 +1,12 @@
 # Add implementation
 def add(x,y):
     pass
-
+# Subtract implementation
 def subtract(x,y):
     pass
 # Multiplication implementation
 def multiply(x,y):
     pass
-
+# Division implementation
 def divide(x,y):
     pass
