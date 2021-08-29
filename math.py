26acf6f8956daa682234bd21efbd494c35e27b6b
diffapinto <72383435+diffapinto@users.noreply.github.com>
2021-08-29 11:04:38 +0530

    Updateed math.py

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     return x+y
 # Subtract implementation
 def subtract(x,y):
-    return x-y
+    return x-y  # on remote rep
 # Multiplication implementation
 def multiply(x,y):
     return x*y
